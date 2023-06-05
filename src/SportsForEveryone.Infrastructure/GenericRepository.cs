0e01635521bfab4e5fea731d8d59a37a3524a0f4
Nick <nickoleezp1k@outlook.com>
2023-06-05 23:32:28 +0300

    Add GenericRepository and UnitOfWork

---
+++ src/SportsForEveryone.Infrastructure/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsForEveryone.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsForEveryone.Infrastructure
+{
+    public class GenericRepository<TEntity> where TEntity : class
+    {
+        internal DataDbContext _context;
+        internal DbSet<TEntity> _dbSet;
+
+        public GenericRepository(DataDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null, string props = "")
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            if(filter != null) query = query.Where(filter);
+            foreach(var prop in props.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(prop);
+            }
+
+            return query.ToList();
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public virtual TEntity GetById(object id)
+        {
+            return _dbSet.Find(id);
+        }
+    }
+}
---
+++ src/SportsForEveryone.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,132 @@
+﻿using SportsForEveryone.Core.Entities;
+using SportsForEveryone.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsForEveryone.Infrastructure
+{
+    public class UnitOfWork
+    {
+        private DataDbContext _context;
+        private GenericRepository<Activity> _activityRepository;
+        private GenericRepository<ActivityType> _activityTypeRepository;
+        private GenericRepository<Administrator> _administratorRepository;
+        private GenericRepository<Club> _clubRepository;
+        private GenericRepository<Post> _postRepository;
+        private GenericRepository<Review> _reviewRepository;
+        private GenericRepository<Trainer> _trainerRepository;
+
+        public GenericRepository<Activity> ActivityRepository
+        {
+            get
+            {
+                if(_activityRepository == null)
+                {
+                    _activityRepository = new GenericRepository<Activity>(_context);
+                };
+                return _activityRepository;
+            }
+        }
+
+        public GenericRepository<ActivityType> ActivityTypeRepository
+        {
+            get
+            {
+                if(_activityTypeRepository == null)
+                {
+                    _activityTypeRepository = new GenericRepository<ActivityType>(_context);
+                }
+                return _activityTypeRepository;
+            }
+        }
+
+        public GenericRepository<Administrator> AdministratorRepository
+        {
+            get
+            {
+                if(_administratorRepository == null)
+                {
+                    _administratorRepository = new GenericRepository<Administrator>(_context);
+                }
+                return _administratorRepository;
+            }
+        }
+
+        public GenericRepository<Club> ClubRepository
+        {
+            get
+            {
+                if(_clubRepository == null)
+                {
+                    _clubRepository = new GenericRepository<Club>(_context);
+                }
+                return _clubRepository;
+            }
+        }
+
+        public GenericRepository<Post> PostRepository
+        {
+            get
+            {
+                if(_postRepository == null)
+                {
+                    _postRepository = new GenericRepository<Post>(_context);
+                }
+                return _postRepository;
+            }
+        }
+
+        public GenericRepository<Review> ReviewRepository
+        {
+            get
+            {
+                if (_reviewRepository == null)
+                {
+                    _reviewRepository = new GenericRepository<Review>(_context);
+                }
+                return _reviewRepository;
+            }           
+        }
+
+        public GenericRepository<Trainer> TrainerRepository
+        {
+            get
+            {
+                if(_trainerRepository == null)
+                {
+                    _trainerRepository = new GenericRepository<Trainer>(_context);
+                }
+                return _trainerRepository;
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing) 
+        {
+            if(!disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
